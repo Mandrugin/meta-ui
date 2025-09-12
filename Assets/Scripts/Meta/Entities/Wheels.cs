e0ff9a1ac0b47491eb9c5f082a482a9cec15f149
Valeiry <v.mandrugin@erminegames.com>
2025-09-12 18:17:22 +0300

    init

---
+++ Assets/Scripts/Meta/Entities/Wheels.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+    public class Hangar
+    {
+        public List<Vehicle> GetAllVehicles()
+        {
+            throw new Exception("Not implemented");
+        }
+    }
+
+    public class Vehicle
+    {
+        
+    }
+
+
+    public class Wheels
+    {
+    
+    }
+}
---
+++ Assets/Scripts/Meta/UseCases/Angar/VehicleStats.cs
@@ -0,0 +1,7 @@
+namespace UseCases
+{
+    public struct VehicleStats
+    {
+    
+    }
+}
