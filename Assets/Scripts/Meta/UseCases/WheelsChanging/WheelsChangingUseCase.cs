530bb5718cb343aa8b7461e73cad71cb7d31767b
Valeiry <v.mandrugin@erminegames.com>
2025-09-12 19:05:21 +0300

    WIP

--- Assets/Scripts/Meta/UseCases/WheelsChanging/WheelsChangingUseCase.cs
+++ Assets/Scripts/Meta/UseCases/WheelsChanging/WheelsChangingUseCase.cs
@@ -2,6 +2,26 @@ namespace UseCases
 {
     public class WheelsChangingUseCase
     {
+        private WheelsData[] _wheelsData;
+        
+        public WheelsChangingUseCase(WheelsData[] wheelsData)
+        {
+            _wheelsData = wheelsData;
+        }
+        
+        public void TryWheelsOut(int wheelIndex)
+        {
+            
+        }
+
+        public void SetWheels(int wheelIndex)
+        {
+            
+        }
+    }
+
+    public abstract class WheelChangingPresenterBase
+    {
         
     }
 }
