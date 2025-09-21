e7b8284e604c16e917c8b166bf9c438d1e67dbf4
Valeriy <valeriy.mandrugin@gmail.com>
2025-09-21 20:00:58 +0300

    WIP

--- Assets/Scripts/Meta/Views/VehicleView/VehicleView.cs
+++ Assets/Scripts/Meta/Views/VehicleView/VehicleView.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class VehicleView : MonoBehaviour
@@ -6,10 +7,54 @@ public class VehicleView : MonoBehaviour
     [SerializeField] private List<GameObject> leftWheels;
     [SerializeField] private List<GameObject> rightWheels;
     
-    public Transform _wheelSlotFL;
-    public Transform _wheelSlotFR;
-    public Transform _wheelSlotRL;
-    public Transform _wheelSlotRR;
-    
-    
+    [SerializeField] private Transform _wheelSlotFL;
+    [SerializeField] private Transform _wheelSlotFR;
+    [SerializeField] private Transform _wheelSlotRL;
+    [SerializeField] private Transform _wheelSlotRR;
+
+    public void SetWheels(int index)
+    {
+        DeleteWheels();
+
+        var fl = Instantiate(leftWheels[index], _wheelSlotFL);
+        var fr = Instantiate(rightWheels[index], _wheelSlotFR);
+        var rl = Instantiate(leftWheels[index], _wheelSlotRL);
+        var rr = Instantiate(rightWheels[index], _wheelSlotRR);
+    }
+
+    private void DeleteWheels()
+    {
+        foreach (Transform t in _wheelSlotFL)
+            Delete(t.gameObject);
+        
+        foreach (Transform t in _wheelSlotFR)
+            Delete(t.gameObject);
+        
+        foreach (Transform t in _wheelSlotRL)
+            Delete(t.gameObject);
+        
+        foreach (Transform t in _wheelSlotRR)
+            Delete(t.gameObject);
+        
+        void Delete(GameObject g)
+        {
+#if UNITY_EDITOR
+            DestroyImmediate(g);
+#else
+            Destroy(g);
+#endif
+        }        
+    }
+
+    [MenuItem("Custom/Rnadom Wheels")]
+    private static void SetRandomWheels()
+    {
+        FindAnyObjectByType<VehicleView>().SetWheels(Random.Range(0, 5));
+    }
+
+    [MenuItem("Custom/Clean")]
+    private static void Clean()
+    {
+        FindAnyObjectByType<VehicleView>().DeleteWheels();
+    }
 }
