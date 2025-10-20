462b1ce3d51aaba5b4a9ab5b8db0524a009d5aeb
Valeriy <valeriy.mandrugin@gmail.com>
2025-10-20 23:57:20 +0300

    factory bases

---
+++ Assets/Scripts/Meta/UseCases/IFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Meta.UseCases
+{
+    public interface IFactory: IDisposable
+    {
+        void Create();
+        void Destroy();
+    }
+    
+    public interface IHangarFactory: IFactory {}
+    public interface IVehicleFactory: IFactory {}
+    public interface IWheelsChangingFactory: IFactory {}
+}
