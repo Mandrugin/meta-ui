742973dc0d9e0c5bb7a90884b94edceaced685e3
Valeiry <v.mandrugin@erminegames.com>
2025-10-29 12:35:33 +0300

    WIP

--- Assets/Scripts/Editor/BoilerPlateGenerator.cs
+++ Assets/Scripts/Editor/BoilerPlateGenerator.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using PlasticGui;
 using UnityEditor;
 
 public abstract class BoilerPlateGenerator
@@ -11,13 +10,35 @@ public abstract class BoilerPlateGenerator
     private static void Generate()
     {
         CreateView();
+        CreatePresenter();
     }
 
     private static void CreateView()
     {
         var viewFileName = entityName + "View.cs";
         var viewText = File.ReadAllText("Assets/Scripts/Editor/Views/PlaceHolderView.cs");
-        viewText = viewText.Replace("PlaceHolder", entityName);
+        viewText = ReplacePlaceHolder(viewText);
         File.WriteAllText("Assets/Scripts/Meta/Views/" + viewFileName, viewText);
+    }
+
+    private static void CreatePresenter()
+    {
+        var presenterFileName = entityName + "Presenter.cs";
+        var iViewFileName = "I" + entityName + "View.cs";
+        
+        var iViewText = File.ReadAllText("Assets/Scripts/Editor/Presenters/IPlaceHolderView.cs");
+        iViewText = ReplacePlaceHolder(iViewText);
+        File.WriteAllText("Assets/Scripts/Meta/Presenters/" + iViewFileName, iViewText);
+        
+        var presenterText = File.ReadAllText("Assets/Scripts/Editor/Presenters/PlaceHolderPresenter.cs");
+        presenterText = ReplacePlaceHolder(presenterText);
+        File.WriteAllText("Assets/Scripts/Meta/Presenters/" + presenterFileName, presenterText);
+    }
+
+    private static string ReplacePlaceHolder(string toReplace)
+    {
+        toReplace = toReplace.Replace("PlaceHolder", entityName);
+        toReplace = toReplace.Replace("placeHolder", entityNameLowerCase);
+        return toReplace;
     }
 }
