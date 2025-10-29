501ef302540d9c6b2d40b2cf9047f1a21c45f1df
Valeiry <v.mandrugin@erminegames.com>
2025-10-29 12:53:20 +0300

    WIP

--- Assets/Scripts/Editor/BoilerPlateGenerator.cs
+++ Assets/Scripts/Editor/BoilerPlateGenerator.cs
@@ -18,43 +18,49 @@ public abstract class BoilerPlateGenerator
 
     private static void CreateView()
     {
+        Directory.CreateDirectory("Assets/Scripts/Meta/Views/" + entityName);
+        
         var viewFileName = entityName + "View.cs";
         var viewText = File.ReadAllText("Assets/Scripts/Editor/Views/PlaceHolderView.cs");
         viewText = ReplacePlaceHolder(viewText);
-        File.WriteAllText("Assets/Scripts/Meta/Views/" + viewFileName, viewText);
+        File.WriteAllText("Assets/Scripts/Meta/Views/" + entityName + "/" + viewFileName, viewText);
     }
 
     private static void CreatePresenter()
     {
+        Directory.CreateDirectory("Assets/Scripts/Meta/Presenters/" + entityName);
+        
         var presenterFileName = entityName + "Presenter.cs";
         var iViewFileName = "I" + entityName + "View.cs";
         
         var iViewText = File.ReadAllText("Assets/Scripts/Editor/Presenters/IPlaceHolderView.cs");
         iViewText = ReplacePlaceHolder(iViewText);
-        File.WriteAllText("Assets/Scripts/Meta/Presenters/" + iViewFileName, iViewText);
+        File.WriteAllText("Assets/Scripts/Meta/Presenters/" + entityName + "/" + iViewFileName, iViewText);
         
         var presenterText = File.ReadAllText("Assets/Scripts/Editor/Presenters/PlaceHolderPresenter.cs");
         presenterText = ReplacePlaceHolder(presenterText);
-        File.WriteAllText("Assets/Scripts/Meta/Presenters/" + presenterFileName, presenterText);
+        File.WriteAllText("Assets/Scripts/Meta/Presenters/" + entityName + "/" + presenterFileName, presenterText);
     }
 
     private static void CreateUseCase()
     {
+        Directory.CreateDirectory("Assets/Scripts/Meta/UseCases/" + entityName);
+        
         var useCaseFileName = entityName + "UseCase.cs";
         var iPresenterFileName = "I" + entityName + "Presenter.cs";
         var iFactoryFileName = "I" + entityName + "Factory.cs";
         
         var useCaseText = File.ReadAllText("Assets/Scripts/Editor/UseCases/PlaceHolderUseCase.cs");
         useCaseText = ReplacePlaceHolder(useCaseText);
-        File.WriteAllText("Assets/Scripts/Meta/UseCases/" + useCaseFileName, useCaseText);
+        File.WriteAllText("Assets/Scripts/Meta/UseCases/" + entityName + "/" + useCaseFileName, useCaseText);
         
         var iPresenterText = File.ReadAllText("Assets/Scripts/Editor/UseCases/IPlaceHolderPresenter.cs");
         iPresenterText = ReplacePlaceHolder(iPresenterText);
-        File.WriteAllText("Assets/Scripts/Meta/UseCases/" + iPresenterFileName, iPresenterText);
+        File.WriteAllText("Assets/Scripts/Meta/UseCases/" + entityName + "/" + iPresenterFileName, iPresenterText);
         
         var iFactoryText = File.ReadAllText("Assets/Scripts/Editor/UseCases/IPlaceHolderFactory.cs");
         iFactoryText = ReplacePlaceHolder(iFactoryText);
-        File.WriteAllText("Assets/Scripts/Meta/UseCases/" + iFactoryFileName, iFactoryText);
+        File.WriteAllText("Assets/Scripts/Meta/UseCases/" + entityName + "/" + iFactoryFileName, iFactoryText);
     }
 
     private static string ReplacePlaceHolder(string toReplace)
